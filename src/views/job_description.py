83713316fa03dce178a6d35a0278f0fab09c1ea3
Kovalev Vasilii <minskij10@yandex.ru>
2019-06-30 01:38:14 +0300

    Fix job request

--- src/views/job_description.py
+++ src/views/job_description.py
@@ -3,17 +3,18 @@ from flask import jsonify, request
 from hh_api import hh_api
 from redis_db import redis_db
 import json
+from github_api import api
 
-@app.route('/find/employees/')
-def get_employees():
-    data = request.data
 
 @app.route('/find/job/<string:login>')
 def get_jobs(login):
-    res = json.loads(redis_db.hget(login, 'language_counts'))
+    try:
+        res = json.loads(redis_db.hget(login, 'lang_dict_prs'))
+    except:
+        res = api.get_languages_counts(login)
     if res:
         list_d = list(res.items())
         list_d.sort(reverse=True, key=lambda i: i[1])
         key = list(map(lambda pair: pair[0], list_d[:1]))
         return jsonify(hh_api.get_jobs(str(key)))
-    return {}
+    return jsonify([])
