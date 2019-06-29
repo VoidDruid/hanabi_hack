47688424edef3a7c495dd7e638e6e0def983551d
Igor Beschastnov <beschastnovigor@gmail.com>
2019-06-29 10:11:21 +0300

    Update settings.

--- hanabi_hack/celery.py
+++ hanabi_hack/celery.py
@@ -2,9 +2,9 @@ import os
 
 from celery import Celery
 
-from conf import settings
+from hanabi_hack import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hanabi_hack.settings')
 
 app = Celery('celery')
 app.config_from_object(settings, namespace='CELERY')
--- hanabi_hack/settings.py
+++ hanabi_hack/settings.py
@@ -13,28 +13,13 @@ def getenv_bool(name, default=None):
         return False
 
 
-class Level:
-    LOCAL = 'local'
-    DEV = 'dev'
-    PROD = 'prod'
-
-
-RUN_LEVEL = os.getenv('RUN_LEVEL', Level.LOCAL)
-
-if RUN_LEVEL == Level.PROD:
-    from conf.levels.prod import *
-elif RUN_LEVEL == Level.DEV:
-    from conf.levels.dev import *
-else:
-    from conf.levels.local import *
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '4sd-r*rg_s1!&!nz6lchg%&p*jzx@p_@0s5xz$aj)mn0npaiob'
 
-DEBUG = getenv_bool('DEBUG', (RUN_LEVEL != Level.PROD))
+DEBUG = getenv_bool('DEBUG', True)
 
 
 # Application definition
@@ -46,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'candidate_support_web',
+    'hanabi_hack'
 ]
 
 MIDDLEWARE = [
@@ -59,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'conf.urls'
+ROOT_URLCONF = 'hanabi_hack.urls'
 
 TEMPLATES = [
     {
@@ -77,18 +62,18 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'conf.wsgi.application'
+WSGI_APPLICATION = 'hanabi_hack.wsgi.application'
 
 
 # Main database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', DATABASE_NAME),
-        'USER': os.getenv('DB_USER', DATABASE_USER),
-        'PASSWORD': os.getenv('DB_PASSWORD', DATABASE_PASSWORD),
-        'HOST': os.getenv('DB_HOST', DATABASE_HOST),
-        'PORT': os.getenv('DB_PORT', DATABASE_PORT)
+        'NAME': os.getenv('DB_NAME', 'hanabi'),
+        'USER': os.getenv('DB_USER', 'hanabi'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'password'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
@@ -117,42 +102,22 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/candidate-support/static/'
+STATIC_URL = '/hanabi_hack/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Celery config
 CELERY_TIMEZONE = TIME_ZONE
 
-CELERY_BROKER_URL = os.environ.get("BROKER_URL", BROKER_URL)
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", CELERY_RESULT_BACKEND)
+CELERY_BROKER_URL = os.environ.get("BROKER_URL", 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", 'redis://localhost:6379/1')
 
 CELERY_IMPORTS = [
-    "candidate_support_bot.tasks"
+    "tasks"
 ]
 
 CELERY_BEAT_SCHEDULE = {
-    'send-stats': {
-        'task': 'candidate_support_bot.tasks.report_stats',
-        'schedule': crontab(day_of_week=5, hour=18, minute=1)
-    }
 }
 
 RUN_CELERY_TASKS = getenv_bool('RUN_CELERY_TASKS', True)
 if not RUN_CELERY_TASKS:
     CELERY_BEAT_SCHEDULE = {}
-
-# BOT settings
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN', DEFAULT_TELEGRAM_TOKEN)
-MAIN_URL = os.getenv("MAIN_URL", 'testbot.tinkoff.ru')
-WEBHOOK = getenv_bool('WEBHOOK', True)
-
-TG_REDIS_EXPIRATION = int(os.getenv('REDIS_EXPIRATION', DEFAULT_TG_REDIS_EXPIRATION))
-TG_REDIS_HOST = os.getenv('REDIS_HOST', DEFAULT_TG_REDIS_HOST)
-TG_REDIS_PORT = int(os.getenv('REDIS_PORT', DEFAULT_TG_REDIS_PORT))
-TG_REDIS_DB = int(os.getenv('REDIS_DB', DEFAULT_TG_REDIS_DB))
-STATS_REDIS_DB = int(os.getenv('STATS_REDIS_DB', DEFAULT_STATS_REDIS_DB))
-MAIN_ADMIN = int(os.getenv('MAIN_ADMIN', 224613065))
-
-EWS_EMAIL = os.getenv('EWS_MAIL', 'candidate_support_bot@tinkoff.ru')
-EWS_PASSWORD = os.getenv('EWS_PASSWORD')
-EWS_USERNAME = os.getenv('EWS_USERNAME', 'TCSBANK\svc_candidate_sbot')
