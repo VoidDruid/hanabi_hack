d46ab8dd8dc01344e5b51a31a66ab6d5680fe49a
Kovalev Vasilii <minskij10@yandex.ru>
2019-06-29 16:05:14 +0300

    Complete github api

--- src/github_api.py
+++ src/github_api.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 
 class GithubApi:
     def __init__(self):
-        self.g= Github("KovalevVasilii", "witcher136")
+        self.g = Github("KovalevVasilii", "witcher136")
 
     def get_user_repos(self, user_name):
         try:
@@ -12,15 +12,41 @@ class GithubApi:
         except:
             return None
 
-    def get_dict_languages(self, user_name):
+    def get_languages_counts(self, user_name):
         lang_dict = defaultdict(int)
         try:
             for repo in self.get_user_repos(user_name):
                 lang_dict[repo.language] += 1
-            del lang_dict[None]
+            lang_dict.pop(None, None)
         except:
             return None
         return lang_dict
 
+    def get_stars(self, user_name):
+        stars_count = 0
+        try:
+            for repo in self.get_user_repos(user_name):
+                stars_count += repo.stargazers_count
+        except:
+            pass
+
+        return stars_count
+
+    def get_user_events_pr(self, user_name):
+        try:
+            return api.g.search_issues('', involves=user_name, type='pr')
+        except:
+            return None
+
+    def get_prs_by_language(self, user_name):
+        lang_dict = defaultdict(int)
+        events = self.get_user_events_pr(user_name)
+        if events:
+            for ev in events:
+                lang_dict[ev.repository.language] += 1
+            lang_dict.pop(None, None)
+        return lang_dict
+
 
 api = GithubApi()
+#api.get_dict_languages_pull_request('KovalevVasilii')
